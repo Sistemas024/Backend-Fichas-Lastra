96b8f6407d880fb27d2c23f49dcb506286ca1404
Juan Granja <4684593@gmail.com>
2024-11-18 16:22:56 -0500

    vercel.json

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const app = require('./app');
 const connectDB = require('./config/db');
 const cors = require('cors');
 
+// Configuración de CORS
+const corsOptions = {
+  origin: 'https://fichas-tecnicas-lastra.vercel.app', // El origen de tu frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+  allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
+};
+
+// Aplicar CORS a todas las rutas
+app.use(cors(corsOptions));
+
 // Conectar a la base de datos y luego iniciar el servidor
 connectDB().then(() => {
   const PORT = process.env.PORT || 5000;
